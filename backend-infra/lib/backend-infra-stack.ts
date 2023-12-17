07971f736f109596e31dfe9d2288fad3176636c8
Eers <sreeaadhi07@gmail.com>
2023-12-18 01:26:57 +0530

    lambda function update

--- backend-infra/lib/backend-infra-stack.ts
+++ backend-infra/lib/backend-infra-stack.ts
@@ -1,15 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export class BackendInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const layer = new lambda.LayerVersion(this, "BaseLayer", {
+      code: lambda.Code.fromAsset("lambda_base_layer/layer.zip"),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_10],
+    });
+
     const apiLambda = new lambda.Function(this, 'apiLambda', {
-      runtime: lambda.Runtime.PYTHON_3_11,
+      runtime: lambda.Runtime.PYTHON_3_10,
       code: lambda.Code.fromAsset('../backend/'),
       handler: 'api.handler',
+      layers: [layer],
+      environment: {
+        PALM_API_KEY: process.env.PALM_API_KEY || '',
+      }
     });
+
   }
 }
