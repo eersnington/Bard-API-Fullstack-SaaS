66aa486387469a2756dc8cb30e6a4acf698a3ab8
Eers <sreeaadhi07@gmail.com>
2023-12-23 22:15:49 +0530

    Update resultsComponent.tsx

--- frontend/app/components/resultsComponent.tsx
+++ frontend/app/components/resultsComponent.tsx
@@ -11,20 +11,36 @@ interface ResultsComponentProps {
 const ResultsComponent: React.FC<ResultsComponentProps> = (props) => {
   const keywordElements = [];
   for (let i = 0; i < props.keywords.length; i++) {
-    const element = <div key={i}>#{props.keywords[i]}</div>;
+    const element = (
+      <div className="bg-amber-400 p-1 px-2 text-sm rounded-md" key={i}>
+        #{props.keywords[i]}
+      </div>
+    );
     keywordElements.push(element);
   }
+
+  const keywordElementsHolder = (
+    <div className="flex flex-wrap gap-2">{keywordElements}</div>
+  );
   const gradientButtonStyle =
     "w-full p-2 bg-gradient-to-r from-amber-500 to-amber-700";
 
+  const boxStyle =
+    "text-gray-800 font-medium bg-white rounded-md w-full my-4 p-2";
   return (
-    <div className="my-2 py-4">
-      <h2 className="font-semibold">Prompt</h2>
-      <p>{props.prompt}</p>
-      <h2 className="font-semibold">Snippet</h2>
-      <p>{props.snippet}</p>
-      <h2 className="font-semibold">Keywords</h2>
-      {keywordElements}
+    <>
+      <div className={boxStyle}>
+        <h2 className="font-semibold mb-2">Prompt</h2>
+        <p className="font-bold text-lg">{props.prompt}</p>
+      </div>
+      <div className={boxStyle}>
+        <h2 className="font-semibold mb-2">Snippet</h2>
+        <p className="font-bold text-lg">{props.snippet}</p>
+      </div>
+      <div className={boxStyle}>
+        <h2 className="font-semibold mb-2">Keywords</h2>
+        {keywordElementsHolder}
+      </div>
       <button
         className={
           gradientButtonStyle +
@@ -34,7 +50,7 @@ const ResultsComponent: React.FC<ResultsComponentProps> = (props) => {
       >
         Back
       </button>
-    </div>
+    </>
   );
 };
 
